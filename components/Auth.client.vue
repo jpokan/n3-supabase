968d06cdab58a25ad56b79471c1adb295cd26e4b
jpokan <jorgeou1993@gmail.com>
2023-09-28 01:30:45 -0500

    fix: update styles

--- components/Auth.client.vue
+++ components/Auth.client.vue
@@ -1,11 +1,30 @@
 <template>
-	<div>
+	<div class="mx-auto flex flex-col gap-2">
 		<button @click="$supabase.auth.signInWithOAuth({ provider: 'google' })">Login with Google</button>
+		<button @click="$supabase.auth.signInWithOAuth({ provider: 'github' })">Login with Github</button>
 		<button @click="$supabase.auth.signOut()">Logout</button>
 		<NuxtLink to="dashboard">dash</NuxtLink>
 	</div>
 </template>
 
 <script setup lang="ts">
-const { $supabase } = useNuxtApp()
 </script>
+
+<style>
+.gap-2 {
+	gap: 0.3rem;
+}
+
+.flex-col {
+	flex-direction: column;
+}
+
+.flex {
+	display: flex;
+}
+
+.mx-auto {
+	margin-right: auto;
+	margin-right: auto;
+}
+</style>
