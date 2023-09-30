10af8af500988b87e0815a4c7d7f79e04f1282aa
jpokan <jorgeou1993@gmail.com>
2023-09-30 09:48:16 -0500

    fix: update deps config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    modules: ['nuxt-svgo'],
+    css: ['~/assets/css/main.css'],
+    postcss:{
+        plugins:{
+            tailwindcss: {},
+            autoprefixer: {}
+        }
+    },
     runtimeConfig: {
         public: {
             supabaseUrl: process.env.SUPABASE_URL,
